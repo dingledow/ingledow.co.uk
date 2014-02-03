468870bb8b64ce531187b3f70fd332ed5af541a9
dingledow <david@ingledow.co.uk>
2014-02-03 21:36:13 +0000

    test

--- attractiveness-study/q24.php
+++ attractiveness-study/q24.php
@@ -53,40 +53,49 @@ $_SESSION['image8'];
       max: 7,
       step: 1,
       slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value );
-      }
-    });
-    var rangevalue = $( "#amount" ).val( ui.value );
-    console.log(rangevalue);
+        
+      	var rangevalue = $( "#amount" ).val( ui.value );
+    	console.log(rangevalue);
 
         if (rangevalue == 1) {
 			$("img.1").css("display","inline"),
 			$("img.2, img.3, img.4, img.5, img.6, img.7").css("display","none");
+			$( "#amount" ).val(1);
 		}
 		if (rangevalue == 2) {
 			$("img.2").css("display","inline");
 			$("img.1, img.3, img.4, img.5, img.6, img.7").css("display","none");
+			$( "#amount" ).val(2);
 		}
 		if (rangevalue == 3) {
 			$("img.3").css("display","inline");
 			$("img.1, img.2, img.4, img.5, img.6, img.7").css("display","none");
+			$( "#amount" ).val(3);
 		}
 		if (rangevalue == 4) {
 			$("img.4").css("display","inline");
 			$("img.1, img.2, img.3, img.5, img.6, img.7").css("display","none");
+			$( "#amount" ).val(4);
 		}
 		if (rangevalue == 5) {
 			$("img.5").css("display","inline");
 			$("img.1, img.2, img.3, img.4, img.6, img.7").css("display","none");
+			$( "#amount" ).val(5);
 		}
 		if (rangevalue == 6) {
 			$("img.6").css("display","inline");
 			$("img.1, img.2, img.3, img.4, img.5, img.7").css("display","none");
+			$( "#amount" ).val(6);
 		}
 		if (rangevalue == 7) {
 			$("img.7").css("display","inline");
 			$("img.1, img.2, img.3, img.4, img.5, img.6").css("display","none");
+			$( "#amount" ).val(7);
 		}
+
+
+      }
+    });
 });
 
 $("img.4").css("display","inline");
